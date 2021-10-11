ec4c808cd1e7e61dff99e30d0d0a69629efdcb6c
sekaznaika <60899923+sekaznaika@users.noreply.github.com>
2021-10-11 22:08:34 +0300

    Вычисление точки через функцию в мейн

---
+++ sandbox/src/main/java/Distance.java
@@ -0,0 +1,16 @@
+public class Distance {
+    public static void main(String[] args) {
+        Point p1 = new Point(5.0, 7.0);
+        Point p2 = new Point(8.0, 9.0);
+
+        System.out.println(distance(p1, p2));
+
+    }
+
+    public static double distance(Point p1, Point p2) {
+
+        return Math.sqrt(((p2.p1 - p1.p1) * (p2.p1 - p1.p1)) + ((p2.p2 - p1.p2) * (p2.p2 - p1.p2)));
+    }
+
+
+}
