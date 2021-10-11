5949e7a06240833fc687acfd6f994695b3e69bdc
sekaznaika <60899923+sekaznaika@users.noreply.github.com>
2021-10-11 22:38:34 +0300

    реализация метода в классе point

--- sandbox/src/main/java/Distance.java
+++ sandbox/src/main/java/Distance.java
@@ -5,6 +5,8 @@ public class Distance {
 
         System.out.println(distance(p1, p2));
 
+        System.out.println(p1.distance(p1,p2));
+
     }
 
     public static double distance(Point p1, Point p2) {
