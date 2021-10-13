2c7fac40ad6f8553a4c5f209c675201724cadb59
sekaznaika <60899923+sekaznaika@users.noreply.github.com>
2021-10-13 14:29:24 +0300

    Добавлены тесты к Point

---
+++ sandbox/src/test/java/PointTest.java
@@ -0,0 +1,28 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class PointTest {
+
+    @Test
+    public void testDistance() {
+        Point tp = new Point(1.0,3.0);
+        Point tp2 = new Point(3.0,5.0);
+        Assert.assertEquals(tp.distance(tp,tp2), 2.8284271247461903);
+    }
+
+    @Test
+    public void testPointsEqual() {
+        Point tp = new Point(1.0,1.0);
+        Point tp2 = new Point(1.0,1.0);
+        Assert.assertEquals(tp.distance(tp,tp2),0);
+    }
+
+    @Test
+    public void testNullPonts() {
+        Point tp = new Point(0.0,0.0);
+        Point tp2 = new Point(0.0,0.0);
+        Assert.assertEquals(tp.distance(tp,tp2),0);
+    }
+}
