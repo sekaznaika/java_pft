f4b23c7cf0e7e1a41047b7f8992ec792a24789ab
sekaznaika <60899923+sekaznaika@users.noreply.github.com>
2021-10-13 20:14:36 +0300

    Исправление тестов

--- sandbox/src/test/java/PointTest.java
+++ sandbox/src/test/java/PointTest.java
@@ -9,20 +9,20 @@ public class PointTest {
     public void testDistance() {
         Point tp = new Point(1.0,3.0);
         Point tp2 = new Point(3.0,5.0);
-        Assert.assertEquals(tp.distance(tp,tp2), 2.8284271247461903);
+        Assert.assertEquals(tp.distance(tp2), 2.8284271247461903);
     }
 
     @Test
     public void testPointsEqual() {
         Point tp = new Point(1.0,1.0);
         Point tp2 = new Point(1.0,1.0);
-        Assert.assertEquals(tp.distance(tp,tp2),0);
+        Assert.assertEquals(tp.distance(tp2),0);
     }
 
     @Test
     public void testNullPonts() {
         Point tp = new Point(0.0,0.0);
         Point tp2 = new Point(0.0,0.0);
-        Assert.assertEquals(tp.distance(tp,tp2),0);
+        Assert.assertEquals(tp.distance(tp2),0);
     }
 }
