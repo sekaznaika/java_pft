7cd404c8c61db0b4c1cef0f00c5629ebc6db7c3b
sekaznaika <60899923+sekaznaika@users.noreply.github.com>
2021-11-02 17:04:18 +0300

    Добавлен хелпер для теста контактов
    Задание 5

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -0,0 +1,32 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactHelper extends HelperBase{
+
+    public ContactHelper(WebDriver wd) {
+        super(wd);
+    }
+
+    public void returnHomePage() {
+        click(By.linkText("home page"));
+    }
+
+    public void submitChanges() {
+        click(By.name("submit"));
+    }
+
+    public void fillTheForm(ContactData contactData) {
+      type(By.name("firstname"),contactData.getFirstname());
+        type(By.name("middlename"),contactData.getMiddlename());
+        type(By.name("lastname"),contactData.getLastname());
+        type(By.name("address"),contactData.getAddress());
+        type(By.name("email"),contactData.getEmail());
+    }
+
+    public void goToContactForm() {
+        click(By.linkText("add new"));
+    }
+}
