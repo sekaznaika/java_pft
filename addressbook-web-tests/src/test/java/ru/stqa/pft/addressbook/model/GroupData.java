bc988b2faa8d4013aaaa2e134cbfc5da72c93df0
sekaznaika <60899923+sekaznaika@users.noreply.github.com>
2021-11-27 00:57:48 +0300

    Сортировка списков
    Лекция 4.10. Сортировка списков

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -16,7 +16,7 @@ public class GroupData {
     }
 
     public GroupData(String name, String header, String footer) {
-        this.id = 0;
+        this.id = Integer.MAX_VALUE;
         this.name = name;
         this.header = header;
         this.footer = footer;
@@ -49,17 +49,17 @@ public class GroupData {
     public String getFooter() {
         return footer;
     }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GroupData groupData = (GroupData) o;
-        return id == groupData.id && Objects.equals(name, groupData.name);
+        return Objects.equals(name, groupData.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name);
+        return Objects.hash(name);
     }
+
 }
