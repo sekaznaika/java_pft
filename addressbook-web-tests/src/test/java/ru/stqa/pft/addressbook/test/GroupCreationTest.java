aadd7daf22bafa09a1c370a1fd1b015e2c7d693b
sekaznaika <60899923+sekaznaika@users.noreply.github.com>
2021-11-27 00:41:20 +0300

    вычисление максимального идентификатора
    Лекция 4.9. Лямбда-выражения (анонимные функции) и элементы функционального программирования

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupCreationTest.java
@@ -19,13 +19,7 @@ public class GroupCreationTest extends TestBase {
         Assert.assertEquals(after.size(), before.size() + 1);
 
 
-        int max = 0;
-        for (GroupData g : after) {
-            if (g.getId() > max) {
-                max = g.getId();
-            }
-        }
-        group.setId(max);
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
