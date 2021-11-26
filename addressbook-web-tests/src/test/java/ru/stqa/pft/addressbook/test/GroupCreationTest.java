8a70b2738a6f3221baf31e4aa0184f11da4e22c2
sekaznaika <60899923+sekaznaika@users.noreply.github.com>
2021-11-27 00:16:56 +0300

    Поиск группы с макс. ид
    Лекция 4.8. Поиск максимального элемента в коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupCreationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTest extends TestBase {
@@ -12,9 +13,21 @@ public class GroupCreationTest extends TestBase {
     public void testGroupCreation() throws Exception {
         ap.getNavigationHelper().goToGroupPage();
         List<GroupData> before = ap.getGroupHelper().getGroupList();
-        ap.getGroupHelper().createGroup(new GroupData("1", null, null));
+        GroupData group = new GroupData("1", null, null);
+        ap.getGroupHelper().createGroup(group);
         List<GroupData> after = ap.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
+
+
+        int max = 0;
+        for (GroupData g : after) {
+            if (g.getId() > max) {
+                max = g.getId();
+            }
+        }
+        group.setId(max);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 
 
