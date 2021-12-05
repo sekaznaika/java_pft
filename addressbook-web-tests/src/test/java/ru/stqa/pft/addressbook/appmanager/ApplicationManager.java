f61fbae58141d0a4290b3e50ebb1ccb392f91cb0
sekaznaika <60899923+sekaznaika@users.noreply.github.com>
2021-12-05 15:05:18 +0300

    Добавлен сетпроперти

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -37,6 +37,7 @@ public class ApplicationManager {
 
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
         dbHelper = new DbHelper();
+        System.setProperty("webdriver.gecko.driver","C:\\Windows\\System32\\geckodriver.exe");
         if ("".equals(properties.getProperty("selenium.server"))) {
             if (browser.equals(BrowserType.CHROME)) {
                 wd = new ChromeDriver();
